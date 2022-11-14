3f6af5974fca0faff7dc410859b1a3903a5a3f65
радаева юлия <lulka-malish@mail.ru>
2022-11-14 17:47:35 +0300

    Example010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {1,2,3,4,5,6,7,4,8};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        //прерви, если есть одинаковые числа, найди 1
+        break;
+    }
+    //index = index +1;
+    index++;
+}
